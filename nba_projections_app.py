6c2f0289d3113c06165603cdb422fbf7ff07e366
bbannach <112113931+bbannach@users.noreply.github.com>
2022-09-20 14:59:25 -0500

    Update nba_projections_app.py

--- nba_projections_app.py
+++ nba_projections_app.py
@@ -187,7 +187,7 @@ summary = summary.merge(all_playoff_results_df_count,how='left',left_index=True,
 summary.index.name = 'Team'
 summary = summary[['mean','min','max','1stRound','2ndRound','ConferenceFinals','Finals','Champion']]
 
-st.dataframe(summary.sort_values('Champion'),ascending=False,height=750)
+st.dataframe(summary.sort_values('Champion',ascending=False),height=750)
 
 #st.write(plt.boxplot(simulations))
 
